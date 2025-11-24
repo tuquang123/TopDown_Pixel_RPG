ebab6cd2a7943a004f8b36f69673ad0209b6b506
tulq <tulq+nxtgms@geargames.com>
2025-11-24 22:41:02 +0700

    fix skill, star

--- Assets/Scripts/Player/PlayerStatsPopupUI.cs
+++ Assets/Scripts/Player/PlayerStatsPopupUI.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 
 public class PlayerStatsPopupUI : BasePopup
 {
-    [SerializeField] private TextMeshProUGUI statsText;
+    [Header("Stat Texts")]
+    [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI defenseText;
+    [SerializeField] private TextMeshProUGUI speedText;
+    [SerializeField] private TextMeshProUGUI critText;
+    [SerializeField] private TextMeshProUGUI lifestealText;
+    [SerializeField] private TextMeshProUGUI attackSpeedText;
 
     public override void Show()
     {
@@ -17,12 +23,29 @@ public class PlayerStatsPopupUI : BasePopup
 
     private void ShowStats(PlayerStats stats)
     {
-        statsText.text =
-            $"<color=#FFD700>Attack:</color> {stats.attack.Value}\n" +
-            $"<color=#FFD700>Defense:</color> {stats.defense.Value}\n" +
-            $"<color=#FFD700>Speed:</color> {stats.speed.Value}\n" +
-            $"<color=#FFD700>Crit:</color> {stats.critChance.Value}%\n" +
-            $"<color=#FFD700>LifeSteal:</color> {stats.lifeSteal.Value}%\n" +
-            $"<color=#FFD700>AttSpeed:</color> {stats.GetAttackSpeed().ToString("0.0")}";
+        attackText.text =
+            $"<sprite name=\"attack_icon\" color=#FF4D4D> " +
+            $"<color=#FFD700>Attack:</color> {stats.attack.Value}";
+
+        defenseText.text =
+            $"<sprite name=\"defense_icon\" color=#4DA6FF> " +
+            $"<color=#FFD700>Defense:</color> {stats.defense.Value}";
+
+        speedText.text =
+            $"<sprite name=\"speed_icon\" color=#7CFF4D> " +
+            $"<color=#FFD700>Speed:</color> {stats.speed.Value}";
+
+        critText.text =
+            $"<sprite name=\"crit_icon\" color=#FFB84D> " +
+            $"<color=#FFD700>Crit:</color> {stats.critChance.Value}%";
+
+        lifestealText.text =
+            $"<sprite name=\"lifesteal_icon\" color=#C44DFF> " +
+            $"<color=#FFD700>LifeSteal:</color> {stats.lifeSteal.Value}%";
+
+        attackSpeedText.text =
+            $"<sprite name=\"attackspeed_icon\" color=#4DFFE3> " +
+            $"<color=#FFD700>AttSpeed:</color> {stats.GetAttackSpeed():0.0}";
     }
+
 }
